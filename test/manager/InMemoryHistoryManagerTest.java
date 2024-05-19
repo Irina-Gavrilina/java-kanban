b19817b17c2b285a0d7decddc4bb1f593a567bfe
Irina Gavrilina <irina.gavrilina.08@yandex.ru>
2024-05-19 11:16:45 +0700

    Changes have been made to the test/manager/InMemoryHistoryManagerTest.java

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -10,18 +10,19 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
-    private static InMemoryHistoryManager inMemoryHistoryManager;
+
+    HistoryManager historyManager;
 
     @BeforeEach
     public void beforeEach() {
-        inMemoryHistoryManager = new InMemoryHistoryManager();
+        historyManager = Managers.getDefaultHistory();
     }
 
     @Test
     void shouldAddToHistory() {
         Task task = new Task("task", "description");
-        inMemoryHistoryManager.addToHistory(task);
-        List<Task> historyList = inMemoryHistoryManager.getHistory();
+        historyManager.addToHistory(task);
+        List<Task> historyList = historyManager.getHistory();
         assertEquals(1, historyList.size());
         assertEquals(task, historyList.getFirst());
     }
@@ -34,11 +35,11 @@ class InMemoryHistoryManagerTest {
 
         Task task = new Task(title, description);
         task.setId(id);
-        inMemoryHistoryManager.addToHistory(task);
+        historyManager.addToHistory(task);
         task.setTitle("newTitle");
         task.setDescription("newDescription");
-        inMemoryHistoryManager.addToHistory(task);
-        List<Task> historyList = inMemoryHistoryManager.getHistory();
+        historyManager.addToHistory(task);
+        List<Task> historyList = historyManager.getHistory();
         assertEquals(2, historyList.size());
         assertEquals(historyList.getFirst().getId(), id);
         assertEquals(historyList.getFirst().getTitle(), title);
