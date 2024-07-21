fef15018a7ad4ac95318d13d2bbad3d96cf1e48e
Irina-Gavrilina <you@example.com>
2024-07-21 18:31:56 +0700

    Add new class TaskManagerTest, add new exceptions and tests, improve functionality of all the project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import manager.Managers;
 import manager.TaskManager;
 import model.*;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,19 +12,22 @@ public class Main {
 
         TaskManager taskManager = Managers.getDefault();
 
-        Task task1 = new Task("Task1", "Description");
+        Task task1 = new Task("Task1", "Description", LocalDateTime.now(), Duration.ofMinutes(5));
         taskManager.createTask(task1);
-        Task task2 = new Task("Task2", "Description");
+        Task task2 = new Task("Task2", "Description", LocalDateTime.now().plusMinutes(15), Duration.ofMinutes(5));
         taskManager.createTask(task2);
 
         Epic epic1 = new Epic("Epic1", "Description");
         taskManager.createEpic(epic1);
 
-        Subtask subtask1 = new Subtask("Subtask1", "Description", epic1.getId());
+        Subtask subtask1 = new Subtask("Subtask1", "Description",
+                LocalDateTime.now().plusMinutes(30), Duration.ofMinutes(5), epic1.getId());
         taskManager.createSubtask(subtask1);
-        Subtask subtask2 = new Subtask("Subtask2", "Description", epic1.getId());
+        Subtask subtask2 = new Subtask("Subtask2", "Description",
+                LocalDateTime.now().plusHours(1), Duration.ofMinutes(2), epic1.getId());
         taskManager.createSubtask(subtask2);
-        Subtask subtask3 = new Subtask("Subtask3", "Description", epic1.getId());
+        Subtask subtask3 = new Subtask("Subtask3", "Description",
+                LocalDateTime.now().plusHours(2), Duration.ofMinutes(10), epic1.getId());
         taskManager.createSubtask(subtask3);
 
         Epic epic2 = new Epic("Epic2", "Description");
--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,14 +1,24 @@
 package model;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task {
 
     private final List<Integer> subtaskId = new ArrayList<>();
+    private LocalDateTime endTime;
 
     public Epic(String title, String description) {
-        super(title, description);
+        super(title, description, null, Duration.ofMinutes(0));
+    }
+
+    public Epic(Epic epic) {
+        super(epic.getTitle(), epic.getDescription(), null, Duration.ofMinutes(0));
+        this.id = epic.getId();
+        this.endTime = null;
     }
 
     public List<Integer> getSubtaskId() {
@@ -21,20 +31,43 @@ public class Epic extends Task {
 
     public void clearSubtaskId() {
         subtaskId.clear();
+        setStartTime(null);
+        setDuration(Duration.ofMinutes(0));
+        setEndTime(null);
     }
 
     public void removeSubtaskId(Integer id) {
         subtaskId.remove(id);
     }
 
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
     @Override
     public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
+        String startTimeToString = "";
+        String endTimeToString = "";
+        if (startTime != null) {
+            startTimeToString = startTime.format(formatter);
+        }
+        if (endTime != null) {
+            endTimeToString = endTime.format(formatter);
+        }
         return "model.Epic{" +
                 "id='" + getId() + '\'' +
                 ", subtaskId=" + subtaskId +
                 ", title='" + getTitle() +
                 ", description='" + getDescription() + '\'' +
                 ", status=" + getTaskStatus() +
+                ", startTime=" + startTimeToString +
+                ", duration=" + duration.toMinutes() + " мин" +
+                ", endTime=" + endTimeToString +
                 '}';
     }
 }
